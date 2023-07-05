4c265720cc7a72453601bfcc3993c4582ff541e7
PC105401\user <tnwl2341@finger.co.kr>
2023-07-05 15:08:51 +0900

    CommonUtil.jsx 생성, App.jsx 수정

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -8,11 +8,14 @@ import Bottom from "@pages/layout/Bottom";
 import UtilHome from "@pages/utils/UtilHome";
 import StringUtil from "@pages/utils/StringUtil";
 import DateUtil from "@pages/utils/DateUtil";
+import ValidationUtil from "@pages/utils/ValidationUtil";
+import CommonUtil from "@pages/utils/CommonUtil";
 import Component from "@pages/components/ComponentHome";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 
 import { Suspense } from "react";
+
 const App = () => {
   const navigate = useNavigate();
   window.navigate = navigate;
@@ -26,6 +29,8 @@ const App = () => {
         <Route path="/Util/*" element={<UtilHome />}></Route>
         <Route path="/StringUtil/*" element={<StringUtil />}></Route>
         <Route path="/DateUtil/*" element={<DateUtil />}></Route>
+        <Route path="/ValidationUtil/*" element={<ValidationUtil />}></Route>
+        <Route path="/CommonUtil/*" element={<CommonUtil />}></Route>
         <Route path="/Component/*" element={<Component />}></Route>
         {/* 일치하는 라우트가 없는경우 처리 */}
         <Route path="*" element={<NotFound />}></Route>
