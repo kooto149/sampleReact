6e9bd35d3a3a442ec740e979a1a6b2df9d41450d
LAPTOP-FENR8Q5R\finger <changha0321@gmail.com>
2023-07-05 10:24:08 +0900

    no message

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -2,11 +2,12 @@ import FallbackLoader from '@components/loder/FallbackLoader';
 import { Routes, Route,useNavigate } from 'react-router-dom';
 import Header from '@pages/layout/Header'
 import Main from '@pages/layout/Main'
-import SamplePage from '@pages/samplePages/SamplePage'
+import SamplePage from '@pages/samplePages/SamplePageHome'
 import NotFound from '@pages/layout/NotFound'
 import Bottom from '@pages/layout/Bottom'
-import Util from '@pages/utils/Util'
+import Util from '@pages/utils/UtilHome'
 import StringUtil from '@pages/utils/StringUtil'
+import Component from '@pages/components/ComponentHome';
 import "bootstrap/dist/css/bootstrap.min.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 
@@ -23,6 +24,7 @@ const App = () => {
 					<Route path="/SamplePage/*" element={<SamplePage />}></Route>
 					<Route path="/Util/*" element={<Util />}></Route>
 					<Route path="/StringUtil/*" element={<StringUtil />}></Route>
+					<Route path="/Component/*" element={<Component />}></Route>
 					{/* 일치하는 라우트가 없는경우 처리 */}
 					<Route path="*" element={<NotFound />}></Route>
 				</Routes>
